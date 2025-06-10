a9b108c15d6222e9b3e78dc884782e237225a250
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2025-06-10 01:13:30 +0100

    chore: add logging

--- snap/local/opt/karapace/bin/start-wrapper.bash
+++ snap/local/opt/karapace/bin/start-wrapper.bash
@@ -6,4 +6,4 @@ set -e
     --clear-groups \
     --reuid snap_daemon \
     --regid snap_daemon -- \
-    "${SNAP}/bin/karapace" "${SNAP_DATA}"/etc/karapace/karapace.config.json
+    bash -c "\"${SNAP}/bin/karapace\" \"${SNAP_DATA}\"/etc/karapace/karapace.config.json >> \"${SNAP_COMMON}/var/log/karapace/output.log\""
